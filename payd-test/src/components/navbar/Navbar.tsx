4ddab097dadcb6a1974f7953cfe4f2a3a5170082
joykara <joykarani1@gmail.com>
2024-06-13 09:17:26 +0300

    refactor: Update Navbar component to use Chakra UI color mode

--- payd-test/src/components/navbar/Navbar.tsx
+++ payd-test/src/components/navbar/Navbar.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Text, useColorMode } from '@chakra-ui/react';
 
 const Navbar: React.FC = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Box bg="brand.darkGreen" color="white" py={4} px={4} boxShadow="md" mb={{base:"4"}}>
+    <Box bg={colorMode === 'dark' ? 'gray.800' : 'brand.darkGreen'} color="white" py={4} px={4} mb={{base:"4"}}>
       <Flex justify="space-between" align="center">
         <Text fontSize="xl">My Blog</Text>
+        {/* <Button
+          ml={4}
+          onClick={toggleColorMode}
+          bg="brand.amber"
+          color={colorMode === 'dark' ? 'gray.800' : 'brand.darkGreen'}
+          _hover={{ bg: 'brand.white' }}
+          borderRadius={20}
+        >
+          {colorMode === 'dark' ? 'Light' : 'Dark'}
+        </Button> */}
       </Flex>
     </Box>
   );
