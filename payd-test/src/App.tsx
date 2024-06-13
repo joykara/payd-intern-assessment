7a91f31e96d7758b474c15cae3a25a9dfeb9ebd0
joykara <joykarani1@gmail.com>
2024-06-13 08:38:35 +0300

    chore: Update App.js to App.tsx and add Navbar and Posts components

---
+++ payd-test/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import Posts from './components/posts/Posts';
+
+const App = () => {
+
+  return (
+    <>
+      <Navbar />
+      <Posts/>
+    </>
+  );
+}
+
+export default App;
